47de20af99fc9b53ba6826c042b3acb25991bcff
egwolk <erincovacha@gmail.com>
2025-09-01 22:45:28 +0800

    Refactor game scenes and player mechanics
    - Updated level scene structure, moving to main_scenes directory and adjusting player instance.
    - Introduced new player bullet scene with collision and shooting mechanics.
    - Modified player script to handle shooting and bullet instantiation.
    - Adjusted viewport size and project settings for better gameplay experience.
    - Cleaned up unused resources and optimized scene folding configurations.
    - Added new textures for bullets and updated resource paths.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
 @export var speed :float = 200.0
-@export var acceleration :float = 15.0
-#@export var friction :float = acceleration / speed
+@export var acceleration :float = 10.0
 
-#var velocity := Vector2(0,0) 
-#@onready var _ship_sprite = $ship_sprite
+@onready var bullet = preload("res://scenes/player_bullet/bullet.tscn")
+@onready var level = get_parent()
+@onready var _ship = $ship_sprite
 
-func _ready():
-	#position = Vector2(0, 600)
-	#scale = Vector2(.50, .50)
-	pass
+
+func shoot():
+	var instance = bullet.instantiate()
+	instance.dir = rotation
+	instance.spawn = global_position
+	instance.spawnRot = rotation
+	instance.zdex = z_index -1
+	level.add_child.call_deferred(instance)
 	
 #func _process(delta):
 	#if Input.is_action_pressed("ui_right"):
@@ -25,23 +29,21 @@ func _ready():
 	#elif Input.is_action_pressed("ui_down"):
 		#position += Vector2(0, 1) * 70 * delta
 		#print("moving down")
+		
 func get_input(delta: float) -> void:
-	#apply_friction(delta)
 	var input_direction = Input.get_vector("left", "right", "up", "down").normalized()
-	velocity.x = move_toward(velocity.x, input_direction.x * speed, acceleration)
-	velocity.y = move_toward(velocity.y, input_direction.y * speed, acceleration)
+	velocity = velocity.move_toward(input_direction * speed, acceleration)
 	position += velocity * delta
 	
-	#if input_direction.length() == 0:
-		#_ship_sprite.play("booster_off")
-	#else:
-		#_ship_sprite.play("default")
-#func apply_friction(delta: float) -> void:
-	#velocity -= velocity * friction * delta
+	
 
 func _process(delta: float) -> void:
-	get_input(delta)
+	get_input(delta)	
+	position.x = clamp(position.x, 17 , 943)
+	position.y = clamp(position.y, 15 , 525)
 	
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("shoot"):
+		shoot()
 	move_and_slide()
 	move_and_collide(velocity*delta)
